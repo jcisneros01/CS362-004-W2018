7ac97c83733254180859ef97eecd4ed6b3c7df92
jcisneros01 <cisnejos@oregonstate.edu>
2018-03-18 15:14:43 -0700

    added random test

--- projects/cisnejos/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/cisnejos/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,7 @@
 
 
 import junit.framework.TestCase;
+import java.util.concurrent.ThreadLocalRandom;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -13,8 +14,7 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
+
    public void testManualTest()
    {	
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
@@ -56,7 +56,95 @@ public class UrlValidatorTest extends TestCase {
    
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+	   	UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+		String url = "";
+		boolean expected = true;
+		boolean actual;
 
+		for (int i = 1; i < 10; i++) {
+			
+			// generate random URL
+			int randomScheme = ThreadLocalRandom.current().nextInt(0, 8 + 1);
+			int randomAuthority = ThreadLocalRandom.current().nextInt(0, 18 + 1);
+			int randomPort = ThreadLocalRandom.current().nextInt(0, 6 + 1);
+			int randomPath = ThreadLocalRandom.current().nextInt(0, 9 + 1);
+			int randomQuery = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+			
+			url = testUrlScheme[randomScheme].item + testUrlAuthority[randomAuthority].item + testUrlPort[randomPort].item + testPath[randomPath].item + testUrlQuery[randomQuery].item;
+			expected = testUrlScheme[randomScheme].valid & testUrlAuthority[randomAuthority].valid & testUrlPort[randomPort].valid & testPath[randomPath].valid & testUrlQuery[randomQuery].valid;
+			System.out.println(url);
+			
+			//assertion
+			actual = urlVal.isValid(url);
+			assertEquals(url, expected, actual);
+			
+			// print results
+			System.out.println("expected: " + expected);
+			System.out.println("actual: " + actual);
+			System.out.println("\n");
+		}
    }
+   
+   
+   	ResultPair[] testUrlScheme = {
+   		   new ResultPair("http://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("h3t://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)};
+
+	ResultPair[] testUrlAuthority = {
+		  new ResultPair("www.google.com", true),
+          new ResultPair("go.com", true),
+          new ResultPair("go.au", true),
+          new ResultPair("0.0.0.0", true),
+          new ResultPair("255.255.255.255", true),
+          new ResultPair("256.256.256.256", false),
+          new ResultPair("255.com", true),
+          new ResultPair("1.2.3.4.5", false),
+          new ResultPair("1.2.3.4.", false),
+          new ResultPair("1.2.3", false),
+          new ResultPair(".1.2.3.4", false),
+          new ResultPair("go.a", false),
+          new ResultPair("go.a1a", false),
+          new ResultPair("go.cc", true),
+          new ResultPair("go.1aa", false),
+          new ResultPair("aaa.", false),
+          new ResultPair(".aaa", false),
+          new ResultPair("aaa", false),
+          new ResultPair("", false)
+	};
+	ResultPair[] testUrlPort = {
+		 new ResultPair(":80", true),
+         new ResultPair(":65535", true),
+         new ResultPair(":0", true),
+         new ResultPair("", true),
+         new ResultPair(":-1", false),
+         new ResultPair(":65636", true),
+         new ResultPair(":65a", false)
+	};
+	ResultPair[] testPath = {
+		  new ResultPair("/test1", true),
+		  new ResultPair("/t123", true),
+		  new ResultPair("/$23", true),
+		  new ResultPair("/..", false),
+		  new ResultPair("/../", false),
+		  new ResultPair("/test1/", true),
+		  new ResultPair("", true),
+		  new ResultPair("/test1/file", true),
+		  new ResultPair("/..//file", false),
+		  new ResultPair("/test1//file", false)
+	};
+
+
+	ResultPair[] testUrlQuery = {
+	      new ResultPair("?action=view", true),
+          new ResultPair("?action=edit&mode=up", true),
+          new ResultPair("", true)
+	};
+   
 }
